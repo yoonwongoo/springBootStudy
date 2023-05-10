13098f7ac8272223d420f8154f96b3ee0f4b44c0
yoonwongoo <yunyun26@naver.com>
2023-05-10 10:28:26 +0900

    스프링부트의 자동구성으로 변경

--- src/main/java/com/springboot/learning/LearningApplication.java
+++ src/main/java/com/springboot/learning/LearningApplication.java
@@ -1,19 +1,13 @@
 package com.springboot.learning;
 
 
-import com.springboot.config.MySpringBootApplication;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
-
 import javax.annotation.PostConstruct;
 
 
-@MySpringBootApplication
+@SpringBootApplication
 public class LearningApplication {
 
 
